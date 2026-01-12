e285931f47d5b9bfb537fed8eba1fff487c03a67
beetlejuice1801 <lacatedral.a@yandex.ru>
2026-01-12 16:36:42 +0300

    Добавил index, base, about. Подключил переход из навбара к index, about

--- homework_05/app.py
+++ homework_05/app.py
@@ -19,3 +19,36 @@
   - добавьте представление для чтения сущности
   - добавьте представление для создания сущности
 """
+from urllib.request import Request
+
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse, name="index")
+async def index(request: Request):
+    '''Главная страница.'''
+    context = {
+        "request": request,
+        "title": "Главная страница",
+    }
+    return templates.TemplateResponse("index.html", context)
+
+@app.get("/about/", response_class=HTMLResponse, name="about")
+async def about(request: Request):
+    '''Страница о разработчике.'''
+    context = {
+        "request": request,
+        "title": "О разработчике",
+        "student": {"name": "Андрей Переверзев",
+                    "age": 25}
+    }
+    return templates.TemplateResponse("about.html", context)
+
+
+if __name__ == '__main__':
+    uvicorn.run('app:app', host='127.0.0.1', port=8000, reload=True)
